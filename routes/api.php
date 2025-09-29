85a765d76aacc9cbc7e0aef75eac0c84fa46e6b5
MaryA-23 <ayivormarymagdalene@gmail.com>
2025-09-29 16:39:54 -0700

    changing the setup

--- routes/api.php
+++ routes/api.php
@@ -2,9 +2,6 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\VegetableController;
-use App\Http\Controllers\VegetableRequestController;
-use App\Http\Controllers\Api\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -12,31 +9,11 @@ use App\Http\Controllers\Api\AuthController;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
 |
 */
 
-    // Public routes - no authentication required
-    Route::post('/register', [AuthController::class, 'register']);
-    Route::post('/login', [AuthController::class, 'login']);
-    // Protected routes - require valid Sanctum token
-    Route::middleware('auth:sanctum')->group(function () {
-    Route::post('/logout', [AuthController::class, 'logout']);
-    Route::get('/user', [AuthController::class, 'user']);
-    // Example: farming-related routes
-    // Route::apiResource('farms', FarmController::class);
+Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
+    return $request->user();
 });
-
-
-        Route::get('vegetables', [VegetableController::class, 'index']);
-        Route::get('vegetables/{id}', [VegetableController::class, 'show']);
-        Route::middleware('auth:sanctum')->group(function () {
-        Route::post('vegetables', [VegetableController::class, 'store']);
-        Route::put('vegetables/{id}', [VegetableController::class, 'update']);
-        Route::delete('vegetables/{id}', [VegetableController::class, 'destroy']);
-        Route::post('vegetables/{id}/restore', [VegetableController::class, 'restore']);
-        // Requests
-        Route::post('vegetables/{id}/request', [VegetableRequestController::class, 'store']);
-        Route::post('vegetables/{id}/fulfill', [VegetableRequestController::class, 'fulfill']);
-    });
