0d4ed44ab965b28237a62550eff6d9debad85c98
MaryA-23 <ayivormarymagdalene@gmail.com>
2025-09-29 16:53:15 -0700

    adding route

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,11 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\VegetableController;
+use App\Http\Controllers\VegetableRequestController;
+use App\Http\Controllers\Api\AuthController;
+
+
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +22,27 @@ use Illuminate\Support\Facades\Route;
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
+
+  // Public routes - no authentication required
+  Route::post('/register', [AuthController::class, 'register']);
+  Route::post('/login', [AuthController::class, 'login']);
+  // Protected routes - require valid Sanctum token
+  Route::middleware('auth:sanctum')->group(function () {
+  Route::post('/logout', [AuthController::class, 'logout']);
+  Route::get('/user', [AuthController::class, 'user']);
+  // Example: farming-related routes
+  // Route::apiResource('farms', FarmController::class);
+});
+
+
+      Route::get('vegetables', [VegetableController::class, 'index']);
+      Route::get('vegetables/{id}', [VegetableController::class, 'show']);
+      Route::middleware('auth:sanctum')->group(function () {
+      Route::post('vegetables', [VegetableController::class, 'store']);
+      Route::put('vegetables/{id}', [VegetableController::class, 'update']);
+      Route::delete('vegetables/{id}', [VegetableController::class, 'destroy']);
+      Route::post('vegetables/{id}/restore', [VegetableController::class, 'restore']);
+      // Requests
+      Route::post('vegetables/{id}/request', [VegetableRequestController::class, 'store']);
+      Route::post('vegetables/{id}/fulfill', [VegetableRequestController::class, 'fulfill']);
+  });
