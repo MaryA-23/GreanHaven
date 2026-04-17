47794f00293a69508a1f8715c906572b370e34f2
MaryA-23 <ayivormarymagdalene@gmail.com>
2026-04-17 18:14:05 +0000

    adding mising files

---
+++ database/migrations/2026_04_17_181135_add_missing_fields_to_products_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('products', function (Blueprint $table) {
+        $table->decimal('price', 10, 2)->after('status');
+        $table->integer('quantity')->default(0);
+        $table->text('description')->nullable();
+        $table->string('unit')->nullable(); // kg, bunch, crate
+        $table->boolean('is_available')->default(true);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('products', function (Blueprint $table) {
+            $table->dropColumn([
+            'price',
+            'quantity',
+            'description',
+            'unit',
+            'is_available'
+        ]);
+        });
+    }
+};
