ddbc8e315a8c2ead89ebd623cb5276674630eefa
MaryA-23 <ayivormarymagdalene@gmail.com>
2026-04-30 23:47:03 +0000

    working on some bugs

--- app/Http/Controllers/Auth/VerifyEmailController.php
+++ app/Http/Controllers/Auth/VerifyEmailController.php
@@ -6,27 +6,41 @@ use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Auth\Events\Verified;
 use Illuminate\Foundation\Auth\EmailVerificationRequest;
-use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Response;
 
 class VerifyEmailController extends Controller
 {
     /**
      * Mark the authenticated user's email address as verified.
      */
-    public function __invoke(EmailVerificationRequest $request): RedirectResponse
+    // public function __invoke(EmailVerificationRequest $request): RedirectResponse
+    // {
+    //     if ($request->user()->hasVerifiedEmail()) {
+    //         return redirect()->intended(
+    //             config('app.frontend_url').RouteServiceProvider::HOME.'?verified=1'
+    //         );
+    //     }
+
+    //     if ($request->user()->markEmailAsVerified()) {
+    //         event(new Verified($request->user()));
+    //     }
+
+    //     return redirect()->intended(
+    //         config('app.frontend_url').RouteServiceProvider::HOME.'?verified=1'
+    //     );
+    // }
+     public function __invoke(EmailVerificationRequest $request): Response
     {
-        if ($request->user()->hasVerifiedEmail()) {
-            return redirect()->intended(
-                config('app.frontend_url').RouteServiceProvider::HOME.'?verified=1'
-            );
+        if (! $request->user()->hasVerifiedEmail()) {
+            if ($request->user()->markEmailAsVerified()) {
+                event(new Verified($request->user()));
+            }
         }
 
-        if ($request->user()->markEmailAsVerified()) {
-            event(new Verified($request->user()));
-        }
-
-        return redirect()->intended(
-            config('app.frontend_url').RouteServiceProvider::HOME.'?verified=1'
-        );
+        return response(
+            '<h2>Email verified successfully</h2><p>You can now return to Greenhaven and log in.</p>',
+            200
+        )->header('Content-Type', 'text/html');
     }
+    
 }
