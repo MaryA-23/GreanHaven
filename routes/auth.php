f7926fb65d1d4098969c41746e6f279b62f82974
MaryA-23 <ayivormarymagdalene@gmail.com>
2026-05-01 16:06:11 +0000

    let verify

--- routes/auth.php
+++ routes/auth.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\Auth\AuthenticatedSessionController;
 use App\Http\Controllers\Auth\NewPasswordController;
 use App\Http\Controllers\Auth\PasswordResetLinkController;
 use App\Http\Controllers\Auth\RegisteredUserController;
+use App\Http\Controllers\Auth\PublicVerifyEmailController;        
 use Illuminate\Support\Facades\Route;
 
 Route::post('/register', [RegisteredUserController::class, 'store'])
@@ -26,3 +27,7 @@ Route::post('/reset-password', [NewPasswordController::class, 'store'])
 Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])
                 ->middleware('auth')
                 ->name('logout');
+
+Route::get('/email/verify/{id}/{hash}', [PublicVerifyEmailController::class, '__invoke'])
+    ->middleware(['signed'])
+    ->name('verification.verify');
