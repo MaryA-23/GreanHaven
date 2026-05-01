731764ab343b06c98539056aeb232079a2e8b75b
MaryA-23 <ayivormarymagdalene@gmail.com>
2026-05-01 00:18:59 +0000

    working on some bugs

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -18,7 +18,7 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
-    public function boot(): void
+     public function boot(): void
     {
         if (config('app.url')) {
             URL::forceRootUrl(config('app.url'));
