999ad2313ed36e0e502bf98f7a6e7114f1f542e0
MaryA-23 <ayivormarymagdalene@gmail.com>
2026-07-12 23:55:03 +0000

    fixing a bug

--- app/Http/Controllers/Admin/AdminUserController.php
+++ app/Http/Controllers/Admin/AdminUserController.php
@@ -32,13 +32,12 @@ class AdminUserController extends Controller
             ->table($request->website_uuid . '.users')
             ->orderBy('created_at', 'desc')
             ->get();
-            dd($users->first());
-
-                // return response()->json([
-                //     'status' => 'success',
-                //     'total_users' => $users->count(),
-                //     'users' => AdminUserResource::collection($users),
-                // ], 200);
+                dd($users->first());
+                return response()->json([
+                    'status' => 'success',
+                    'total_users' => $users->count(),
+                    'users' => AdminUserResource::collection($users),
+                ], 200);
             } catch (\Exception $e) {                
                 return response()->json([
                     'status' => 'failed',
