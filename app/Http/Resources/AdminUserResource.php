52b8b8f759f3e84f5017354ccf49f8d1d81bbeb4
MaryA-23 <ayivormarymagdalene@gmail.com>
2026-07-12 23:48:03 +0000

    fixing a bug

--- app/Http/Resources/AdminUserResource.php
+++ app/Http/Resources/AdminUserResource.php
@@ -9,13 +9,17 @@ class AdminUserResource extends JsonResource
     public function toArray($request)
     {
         return [
-            'id' => $this->id,
-            'name' => $this->name,
-            'first_name' => $this->first_name,
-            'last_name' => $this->last_name,
-            'email' => $this->email,
-            'role' => $this->role,
-            'created_at' => $this->created_at,
+        'id' => $this->id,
+        'uuid' => $this->uuid,
+        'name' => $this->name,
+        'first_name' => $this->first_name,
+        'last_name' => $this->last_name,
+        'email' => $this->email,
+        'phone' => $this->phone,
+        'role' => $this->role,
+        'status' => $this->status,
+        'last_login' => $this->last_login,
+        'created_at' => $this->created_at,
         ];
     }
 }
