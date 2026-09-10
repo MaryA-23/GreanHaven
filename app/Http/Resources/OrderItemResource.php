eeac3c9851a7c65f5ca52388020f163984aa87ca
MaryA-23 <ayivormarymagdalene@gmail.com>
2026-09-10 17:18:17 +0000

    working on the notification

--- app/Http/Resources/OrderItemResource.php
+++ app/Http/Resources/OrderItemResource.php
@@ -11,11 +11,22 @@ class OrderItemResource extends JsonResource
     {
         return [
             'id' => $this->id,
+
             'vegetable' => [
                 'id' => $this->vegetable->id,
                 'name' => $this->vegetable->name,
                 'price' => $this->vegetable->price,
+
+                'image' => $this->vegetable->image,
+
+                'image_url' => $this->vegetable->image
+                    ? asset(
+                        'storage/' .
+                        ltrim($this->vegetable->image, '/')
+                    )
+                    : null,
             ],
+
             'quantity' => $this->quantity,
             'price' => $this->price,
             'subtotal' => $this->subtotal,
